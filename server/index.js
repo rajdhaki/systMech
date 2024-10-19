c9eefd65b353de5df87575f31e34f86d368bcd95
RAJ DHAKI <105407783+rajdhaki@users.noreply.github.com>
2024-10-19 23:45:38 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 8000
 const app = express()
 dbConnect()
 app.use(cors({
-	origin: 'http://localhost:5173',
+	origin: '',
 	credentials: true
 }))
 app.use(express.json())
