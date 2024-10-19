8d2e08b1a00b5c4f2e8f544a3b1ec4c9407fb589
rajdhaki <rajdhakibdr@gmail.com>
2024-10-20 00:14:35 +0530

    update index.js

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 8000
 const app = express()
 dbConnect()
 app.use(cors({
-	origin: 'http://localhost:5173',
+	origin: process.env.FRONTEND_URL || '',
 	credentials: true
 }))
 app.use(express.json())
