7564ab44aabb1b3b0e2dfc9c6b832cc131886f96
rajdhaki <rajdhakibdr@gmail.com>
2024-10-20 10:37:40 +0530

    update

--- server/index.js
+++ server/index.js
@@ -36,6 +36,14 @@ app.use(cors({
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/uploads', express.static('uploads'))
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, ")
+    Accept, x-client-key, x-client-token, x-client-secret, Authorization;
+    next();
+});
 
 app.get('/', (req, res) => {
     res.send("Hello there")
